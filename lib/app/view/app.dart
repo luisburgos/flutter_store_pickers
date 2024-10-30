cce083b39ff5f5d4e13567896cdd8d4dcd6b6f55
Luis Burgos <luisarturo.burgos@gmail.com>
2024-10-30 16:25:43 -0600

    feat: add shadcn depedency

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -2,6 +2,8 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_store_pickers/app/app_routes.dart';
 import 'package:flutter_store_pickers/l10n/l10n.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -19,17 +21,32 @@ class _StorePickersApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    const colorSchemeName = 'slate';
+    final textTheme = ShadTextTheme.fromGoogleFont(
+      GoogleFonts.inter,
+    );
+
+    return ShadApp.router(
       routerConfig: defaultAppRouter,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        useMaterial3: true,
-      ),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      themeMode: ThemeMode.light,
+      darkTheme: ShadThemeData(
+        brightness: Brightness.dark,
+        colorScheme: ShadColorScheme.fromName(
+          colorSchemeName,
+          brightness: Brightness.dark,
+        ),
+        textTheme: textTheme,
+      ),
+      theme: ShadThemeData(
+        brightness: Brightness.light,
+        colorScheme: ShadColorScheme.fromName(
+          colorSchemeName,
+        ),
+        textTheme: textTheme,
+      ),
     );
   }
 }
